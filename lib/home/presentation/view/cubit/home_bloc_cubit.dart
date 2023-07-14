cec53a808f8cfec92e28418b70912ebb4111be20
Kevin Alexa <kevinalexa20@gmail.com>
2023-07-14 22:52:50 +0700

    add controller homepage

--- lib/home/presentation/view/cubit/home_bloc_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-
-part 'home_bloc_state.dart';
-
-class HomeBlocCubit extends Cubit<HomeBlocState> {
-  HomeBlocCubit() : super(HomeBlocInitial());
-}
--- lib/home/presentation/view/cubit/home_bloc_state.dart
+++
@@ -1,6 +0,0 @@
-part of 'home_bloc_cubit.dart';
-
-@immutable
-abstract class HomeBlocState {}
-
-class HomeBlocInitial extends HomeBlocState {}
